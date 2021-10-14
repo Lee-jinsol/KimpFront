f6d029c2e7b50f292eb38286d3d5d804c33126af
이진솔 <>
2021-10-14 22:22:44 +0900

    Add pages ( login, register )

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,11 +1,206 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {
+  Form,
+  Input,
+  InputNumber,
+  Cascader,
+  Select,
+  Row,
+  Col,
+  Checkbox,
+  Button,
+  AutoComplete,
+} from 'antd';
+
+const {Option} = Select;
+
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
+const inputform = {
+  width: '600px',
+};
+
+const regisform = {
+  position: 'absolute',
+  top: '40%',
+  left: '50%',
+  transform: 'translate(-50%,-50%)',
+};
 
 function Register() {
-    return (
-        <div>
-            회원가입
-        </div>
-    )
-}
+  const [form] = Form.useForm();
 
-export default Register
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  const [autoCompleteResult, setAutoCompleteResult] = useState([]);
+
+  const onWebsiteChange = (value) => {
+    if (!value) {
+      setAutoCompleteResult([]);
+    } else {
+      setAutoCompleteResult(
+        ['.com', '.org', '.net'].map((domain) => `${value}${domain}`)
+      );
+    }
+  };
+
+  const websiteOptions = autoCompleteResult.map((website) => ({
+    label: website,
+    value: website,
+  }));
+  return (
+    <div style={regisform}>
+      <Form
+        style={inputform}
+        {...formItemLayout}
+        form={form}
+        name="register"
+        onFinish={onFinish}
+        scrollToFirstError
+      >
+        <Form.Item
+          name="email"
+          label="E-mail"
+          rules={[
+            {
+              type: 'email',
+              message: '이메일의 형식이 맞지 않습니다.',
+            },
+            {
+              required: true,
+              message: '이메일을 입력해주세요.',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          label="Password"
+          rules={[
+            {
+              required: true,
+              message: '비밀번호를 입력해주세요.',
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="confirm"
+          label="Confirm Password"
+          dependencies={['password']}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: '비밀번호를 다시 한번 입력해주세요.',
+            },
+            ({getFieldValue}) => ({
+              validator(_, value) {
+                if (!value || getFieldValue('password') === value) {
+                  return Promise.resolve();
+                }
+
+                return Promise.reject(
+                  new Error('The two passwords that you entered do not match!')
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="name"
+          label="Name"
+          rules={[
+            {
+              required: true,
+              message: '이름을 입력해주세요.',
+              whitespace: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          name="phone"
+          label="Phone Number"
+          rules={[
+            {
+              required: true,
+              message: '전화번호를 입력해주세요.',
+            },
+          ]}
+        >
+          <Input
+            style={{
+              width: '100%',
+            }}
+          />
+        </Form.Item>
+
+        <Form.Item
+          name="agreement"
+          valuePropName="checked"
+          rules={[
+            {
+              validator: (_, value) =>
+                value
+                  ? Promise.resolve()
+                  : Promise.reject(new Error('개인정보 활용에 동의 해주세요.')),
+            },
+          ]}
+          {...tailFormItemLayout}
+        >
+          <Checkbox>
+            개인정보 활용에 동의합니다. <a href=""></a>
+          </Checkbox>
+        </Form.Item>
+        <Form.Item {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit">
+            Register
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+}
+export default Register;
