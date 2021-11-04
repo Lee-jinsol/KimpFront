6906d39220e9862df7e39e255c50effa4b09c23a
이진솔 <jinsol9419@naver.com>
2021-11-04 18:51:37 +0900

    넘버 찌그러지는거 수정 1차

--- src/components/Home/AboutCard.jsx
+++ src/components/Home/AboutCard.jsx
@@ -3,10 +3,21 @@ import styled from 'styled-components';
 import Half from '../Layout/Half';
 import CardImg1 from '../../image/CardImg1.png';
 import CardImg2 from '../../image/CardImg2.png';
+import Number from '../Home/Number';
 
 function AboutCard() {
   const BREAK_POINT_MEDIUM = 768;
   const BREAK_POINT_SMALL = 576;
+  const BREAK_POINT_LARGE = 992;
+
+  const Block = styled.div`
+    width: 100%;
+    display: block;
+    padding: 20px;
+    @media only screen and (max-width: ${BREAK_POINT_LARGE}px) {
+      padding: 0;
+    }
+  `;
 
   const Card = styled.section`
     position: relative;
@@ -31,26 +42,54 @@ function AboutCard() {
       }
     }
   `;
+  const Division = styled.div`
+    display: flex;
+    h2 {
+      color: #fff;
+    }
+    p {
+      color: #c9c9c9;
+    }
+  `;
+
+  const Line = styled.div`
+    border-left: 1px solid #fff;
+    height: 40px;
+    margin: 20px;
+    @media only screen and (max-width: ${BREAK_POINT_LARGE}px) {
+      display: none;
+    }
+  `;
+
   return (
     <Card>
-      <Half style={{paddingTop: '100px', borderBottom: '1px solid #e7e7e77e'}}>
-        <div>
-          <h1>KIMP에서 M&A를 진행해야 하는 이유</h1>
-          <p>
-            <b> 1. 안전한 정보공유 </b>
-            <br />
-            - 한국무역협회에서 분사한 M&A플랫폼
-            <br />
-            <b> 2. 다양한 매물 </b>
-            <br />
-            - M&A 생태계 다양한 관계자들을 포용
-            <br />
-            <b> 3. 딱맞는 연결 </b>
-            <br />
-            - 최적의 전문가 및 네트워킹을 통한 선택
-            <br />
-          </p>
-        </div>
+      <Half>
+        <Block>
+          <Division>
+            <Number black>1</Number>
+            <div style={{paddingLeft: '30px'}}>
+              <h2>안전한 정보공유</h2>
+              <p>한국무역협회에서 분사한 M&A플랫폼</p>
+            </div>
+          </Division>
+          <Line />
+          <Division>
+            <Number black>2</Number>
+            <div style={{paddingLeft: '30px'}}>
+              <h2>다양한 매물</h2>
+              <p>M&A 생태계 다양한 관계자들을 포용</p>
+            </div>
+          </Division>
+          <Line />
+          <Division>
+            <Number black>3</Number>
+            <div style={{paddingLeft: '30px'}}>
+              <h2>딱맞는 연결</h2>
+              <p>최적의 전문가 및 네트워킹을 통한 선택</p>
+            </div>
+          </Division>
+        </Block>
+
         <img src={CardImg1} alt="카드1" />
       </Half>
 
--- src/components/Home/Number.jsx
+++ src/components/Home/Number.jsx
@@ -1,27 +1,34 @@
-import styled, { css } from 'styled-components'
+import styled, {css} from 'styled-components';
 
 const BREAK_POINT_SMALL = 576;
 
 const Number = styled.div`
-    border-radius: 50%;
-    width: 50px;
-    height: 50px;
-    background: #102A3E;
-    color: #fff;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 15px;
-    font-weight: bold;
-    ${props => props.white && 
-        css`
-        background: #fff;
-        color: #102A3E;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  background: #102a3e;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 15px;
+  font-weight: bold;
+  ${(props) =>
+    props.white &&
+    css`
+      background: #fff;
+      color: #102a3e;
+    `}
+  ${(props) =>
+    props.black &&
+    css`
+      background: #102a3e;
+      color: #fff;
     `}
     @media only screen and (max-width: ${BREAK_POINT_SMALL}px) {
-        width: 30px;
-        height: 30px;
-    }
-`
+    width: 30px;
+    height: 30px;
+  }
+`;
 
-export default Number
+export default Number;
